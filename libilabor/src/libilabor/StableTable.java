26468f3fef827d79cb33165089703afd4474a2c4
bozobv <61810357+bozobv@users.noreply.github.com>
2020-03-29 00:36:50 +0100

    Update StableTable.java

--- libilabor/src/libilabor/StableTable.java
+++ libilabor/src/libilabor/StableTable.java
@@ -1,6 +1,13 @@
 package libilabor;
 
+import java.util.List;
+
 public class StableTable extends IceTable 
 {
+	public StableTable( int newsnow) {
+		super(newsnow);
+		// TODO Auto-generated constructor stub
+	}
+
 	void PlayerVisit(Player p) {}
 }
