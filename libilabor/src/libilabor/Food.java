7bff901e0b055d84cf934856f0e00a37a13d0699
maacsabai <csabai.m.88@gmail.com>
2020-03-29 21:45:16 +0200

    faszom food

--- libilabor/src/libilabor/Food.java
+++ libilabor/src/libilabor/Food.java
@@ -2,5 +2,18 @@ package libilabor;
 
 public class Food extends Instant 
 {
+
+	public Food(int id){
+		super(id);
+		
+	}
+	
+	//  A food eggyel noveli a THP-t
+	void PickedUpBy(Player p) {
+		p.setThp(p.getThp()+1);
+		
+	}
+
+
 	void pickedUpBy(Player p) {}
 }
