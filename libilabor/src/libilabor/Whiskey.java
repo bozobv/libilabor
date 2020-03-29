9404ee6a0715dc178dda5fb18a114fd772277119
maacsabai <csabai.m.88@gmail.com>
2020-03-29 21:57:02 +0200

    whiskey rablo

--- libilabor/src/libilabor/Whiskey.java
+++ libilabor/src/libilabor/Whiskey.java
@@ -2,5 +2,17 @@ package libilabor;
 
 public class Whiskey extends Instant 
 {
+
+	public Whiskey(int id){
+		super(id);
+		
+	}
+	
+	// A Whiskey kettovel noveli a THP-t.   
+	void PickedUpBy(Player p) {
+		p.setThp(p.getThp()+2);
+		
+	}
 	void pickedUpBy(Player p) {}
+
 }
