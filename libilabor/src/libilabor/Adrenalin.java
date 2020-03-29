bdaaaa51ba9741fb3078d1cac101dd5e977e3def
maacsabai <csabai.m.88@gmail.com>
2020-03-29 22:13:08 +0200

    szaok ha baszok

--- libilabor/src/libilabor/Adrenalin.java
+++ libilabor/src/libilabor/Adrenalin.java
@@ -8,7 +8,7 @@ public class Adrenalin extends Instant
 		
 	}
 	
-	// Az Adrenalin megnovel a munkat. . ...jjjjj
+	// Az Adrenalin megnovel a munkat. . ...jjjjj dsdsd
 	void PickedUpBy(Player p) {
 		p.setWork(p.getWork()+1);
 	}
