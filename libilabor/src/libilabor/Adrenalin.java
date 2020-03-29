e6b51f773cecea4cded08b6d7a2b01e6f5fdfe0b
maacsabai <csabai.m.88@gmail.com>
2020-03-29 21:22:03 +0200

    adrenalin genyo

--- libilabor/src/libilabor/Adrenalin.java
+++ libilabor/src/libilabor/Adrenalin.java
@@ -2,5 +2,17 @@ package libilabor;
 
 public class Adrenalin extends Instant 
 {
+
+	public Adrenalin(int id){
+		super(id);
+		
+	}
+	
+	// Az Adrenalin megnovel a munkat. 
+	void PickedUpBy(Player p) {
+		p.setWork(p.getWork()+1);
+	}
+
 	void pickedUpBy(Player p) {}
+
 }
