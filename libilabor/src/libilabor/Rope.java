c124c7431ffccfeaa6a2cd49505a4fbbe6623c5b
Tierra Geovanny <geovanny@windowslive.com>
2020-04-28 16:51:52 +0200

    rope siker

--- libilabor/src/libilabor/Rope.java
+++ libilabor/src/libilabor/Rope.java
@@ -28,11 +28,11 @@ public class Rope extends Storable implements java.io.Serializable
 	 * @param user a jatekos aki hasznalja a tarygat
 	 */
 	public void used(Player user,int idx) {
-		IceTable it=user.getCurrentTable().getIceField().getIceTables().get(idx);
+		IceTable it =user.getCurrentTable().getIceField().getIceTables().get(idx);
 		ArrayList<IceTable> neighbours = user.getCurrentTable().getNeighbours();
 		if(neighbours.contains(it)){
 			if(it.getPlayersOnTable().size()>0){
-				it.getPlayersOnTable().get(0).setCurrentTable(user.getCurrentTable());
+				user.getCurrentTable().playerVisit(it.getPlayersOnTable().get(0));
 			}
 			else{
 				System.out.println("nincs jatekos a kivalasztott tablan");
