e1bd4c0b3c5f481eb234a49cf822aa79e7dc9956
Márton Bidlek <bidlekmarton@gmail.com>
2020-04-29 10:43:54 +0200

    osszehasonlito

--- libilabor/src/libilabor/Test.java
+++ libilabor/src/libilabor/Test.java
@@ -1,5 +1,6 @@
 package libilabor;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -8,6 +9,13 @@ public class Test {
 
     public static void main(String[] args)
     {
+    	CompareTexts ct= new CompareTexts();
+    	try {
+			ct.compare();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     	CommandHandler ch = new CommandHandler();
     	ch.handling();
     }
