c9a909331f67e107d9e234fa1ab7687a72b4239c
aprod12 <apro.daniel00@gmail.com>
2020-05-17 16:04:36 +0200

    21

--- libilabor/src/Graphics/EndGame.java
+++ libilabor/src/Graphics/EndGame.java
@@ -1,7 +1,36 @@
 package Graphics;
 
-public class EndGame 
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+public class EndGame implements ActionListener
 {
+	private JFrame frame  = new JFrame("Game Over");
+	private Container things = new Container();
+	private JButton restart = new JButton();
+	private JButton exit = new JButton();
+	private JLabel title = new JLabel();
 	void initialize()
-	{}
+	{
+		frame.setSize(new Dimension(300,300));
+		restart.setText("Restart");
+		exit.setText("Exit");
+		things.add(title);
+		things.add(restart);
+		things.add(exit);
+		
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+	if(e.getSource().equals(exit)) {		
+		frame.dispose();
+	}
+		
+	}
 }
