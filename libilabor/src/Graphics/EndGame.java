1d247889cd3267acee61e0a96b498502f2612137
Márton Bidlek <bidlekmarton@gmail.com>
2020-05-17 15:10:48 +0200

    lol

--- libilabor/src/Graphics/EndGame.java
+++ libilabor/src/Graphics/EndGame.java
@@ -1,7 +1,23 @@
 package Graphics;
+import java.awt.Color;
+import java.awt.Dimension;
 
-public class EndGame 
-{
-	void initialize()
-	{}
+import javax.swing.*;
+
+public class EndGame {
+	private JFrame frame;
+	private JLabel iras;
+	public EndGame() {
+		
+		
+		
+		
+		frame=new JFrame("Eskibros");
+		frame.setPreferredSize(new Dimension(500,500));
+		frame.pack();
+	    frame.setVisible(true);
+	    frame.getContentPane().setBackground(new Color(179, 228, 233));
+	    frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+	    frame.setResizable(false);
+	}
 }
