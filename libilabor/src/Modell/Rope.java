b8f14cab94286b60bdc0611bc269923d33986429
Tierra Geovanny <geovanny@windowslive.com>
2020-05-18 20:57:35 +0200

    rope

--- libilabor/src/Modell/Rope.java
+++ libilabor/src/Modell/Rope.java
@@ -42,45 +42,7 @@ public class Rope extends Storable implements java.io.Serializable
 			System.out.println("a kivalasztott tabla tul messze van");
 		}
 		user.removeFromInventory(1);
-		/*boolean done=false;
-		while(!done){
-			ArrayList<Character> characters = null;
-			System.out.println("Melyik szomszedot szeretned kimenteni?( (j)obb,(b)al,(f)enti,(l)enti),(m)egse ");
-			Scanner s=new Scanner(System.in);
-			String input = s.nextLine();
-			if(input.equals("m")){
-				done=true;
-			}
-			else if(input.equals("j")){
-				characters = new ArrayList<>();
-				characters.addAll(neighbours.get(0).getPlayersOnTable());			
-			}
-			else if(input.equals("b")){
-				characters = new ArrayList<>();
-				characters.addAll(neighbours.get(1).getPlayersOnTable());	
-			}
-			else if(input.equals("l")){
-				characters = new ArrayList<>();
-				characters.addAll(neighbours.get(2).getPlayersOnTable());
-			}
-			else if(input.equals("f")){
-				characters = new ArrayList<>();
-				characters.addAll(neighbours.get(3).getPlayersOnTable());
-			}
-			else{
-				System.out.println("Helytelen input");
-			}
-			if(characters!=null){
-				if (characters.size()==0){
-					System.out.println("Nincs senki ezen a mezon");
-				}
-				else{
-					characters.get(0).setCurrentTable(user.getCurrentTable());
-					done=true;
-				}
-			}
-			s.close();
-		}*/
+
 	}
 
 
