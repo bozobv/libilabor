1c91431d96fee6fcfd51b546e7e43956da0a2824
maacsabai <csabai.m.88@gmail.com>
2020-05-18 23:28:30 +0200

    imodell fele

--- libilabor/src/Controller/IModell.java
+++ libilabor/src/Controller/IModell.java
@@ -57,30 +57,94 @@ public interface IModell {
 	
 	public void createIceField(String type, int height, int width);
 	
+	/**
+	 * instabil tabla hozzaadasa
+	 * @param cap kapacitas
+	 * 
+	 */
+	
 	public void addUnstableTable(int cap);
 
+	/**
+	 * tabla hozzadasa
+	 * @param type table tipusa
+	 
+	 
+	 */
+	
 	public void addTable(String type);
-
+	/**
+	 * szomszedok beallitasa
+	 * @param index1 egyik szomszed indexe
+	 * @param index2 a masik szomszed indexe
+	 */
 	public void setNb(int index1, int index2);
+	/**
+	 * ho magassaganak beallitasa
+	 * @param index  a icetable indexe
+	 * @param height a magassag
+	 * */
 
 	public void setSnow(int index, int height);
+	/**
+	 * item beallitasa az icetableban
+	 * @param type a tipusa az itemnek
+	 * @param indexe a tablenek
+	 * */
 	
 	public void setItem(String type, int index);
 
+	/**
+	 * Egy item osszetorese
+	 * @parma index hogy melyik icetablen van
+	 * */
+	
 	public void destroyItem(int index);
 	
+	/**
+	 * A szomszed lekerdezesere
+	 * @param index 
+	 * */
 	public void getNb(int index);
 
+	/**
+	 * player mozgatasa
+	 * @param a jatekos neve aki mozdulni akar
+	 * @param index hogy hova
+	 * */
 	public void movePlayer(String name, int index);
 	
+	/**medve mozgasa
+	 * @param honnan 
+	 * @param hova
+	 * 
+	 * */
 	public void moveBear(int honnan, int hova);
 	
+	/**
+	 * Egy dolog felvetele
+	 * @param name a jatekos neve
+	 * */
+	
 	public void pickUp(String name) ;
+	/**
+	 * item hozzadasa a jatekoshoz
+	 * @param type item tipusa
+	 * @param name a jatekos neve 
+	 * */
 
 	public void addItem(String type, String name);
-	
+	/**
+	 * item eltavolitasa
+	 * @param type tipus
+	 * @param name a jatekos neve
+	 * */
 	public void removeItem(String type, String name);
 
+	/**
+	 * a karakter megolese
+	 * @param name a jatekos neve
+	 * */
 	public void killCharacter(String name);
 	
 	/**
