8922af14578ac806395a8e1f3f30015d106f5d6e
Márton Bidlek <bidlekmarton@gmail.com>
2020-05-18 23:31:48 +0200

    ASD

--- libilabor/src/Controller/IModell.java
+++ libilabor/src/Controller/IModell.java
@@ -46,38 +46,88 @@ public interface IModell {
 	public void removeItem(String type, String name);
 
 	public void killCharacter(String name);
-	
+	/**
+	 * Asas parancs
+	 * @param name a jatekos neve aki as
+	 */
 	public void dig(String name) ;
-
+	/**
+	 * A kotel hasznalat parancs
+	 * @param name A player neve aki hasznalja 
+	 * @param index A tabla, amelyen hasznalni szeretnenk, az elso embert menti meg a tablarol
+	 */
 	public void rope(String name, int index);
-
+	/**
+	 * Felallitja a satrat
+	 * @param name A jatekos aki satrat allit
+	 */
 	public void setTent(String name);
-
+	/**
+	 * A jelzopiusztoly osszeszereles indito fuggveny
+	 * @param name A jatekos aki kezdemenyezi az osszeszerelest 
+	 */
 	public void repairFlareGun(String name) ;
-
+	/**
+	 * Iglo epites a mezore parancs
+	 * @param name Az eszkimo aki epit a mezore
+	 */
 	public void buildIgloo(String name);
-
+	/**
+	 * A sarkkutato kepessegnek parancsa
+	 * @param name A kutato neve
+	 * @param index	A mezo indexe, amelyet fel akar deriteni
+	 */
 	public void scout(String name, int index) ;
-
+	/**
+	 * Thp setter parancs
+	 * @param name Akinek az eletet allitjuk
+	 * @param thp Az uj erteke a thp-nak
+	 */
 	public void setThp(String name, int thp );
-
+	/**
+	 * Work setter parancs
+	 * @param name Akinek a Workjet allitjuk
+	 * @param work Az uj erteke a work-nek
+	 */
 	public void setWork(String name, int work);
-
+	/**
+	 * Hovihar imitalasanak parancsa
+	 * @param size Amennyi palyat erint a hovihar
+	 */
 	public void callBlizzard(int size) ;
-
+	/**
+	 * Aktualis jatekallas mentese parancs
+	 * @param saveFileName A fajl neve ahova menteni akrunk
+	 */
 	public void save(String saveFileName);
-	
+	/**
+	 * A jatek betoltesenek parancsa 
+	 * @param saveFileName A fajl neve ahonnan betoltjuk
+	 */
 	public void load(String saveFileName);
-	
+	/**
+	 * Medve eltavolitas parancs
+	 * @param index A mezo indexe ahonnan a medvet el akarjuk tavolitani
+	 */
 	public void killBear(int index);
-
+	/**
+	 * A jatek kiirasat megvalosito parancs
+	 * @return A jatek kiirasa stringkent
+	 */
 	public String gameStance();
-	
+	/**
+	 * Visszaadja a soron levo jatekost
+	 * @return A soron levo jatekos
+	 */
 	public Player getCurrentPlayer();
-	
+	/**
+	 * Kovetkezo soron levo jatekosra valtas
+	 */
 	public void nextPlayer();
-
-
+	/**
+	 * IceField controlleret allitja be
+	 * @param controller a beallitando ertek
+	 */
 	void setIfController(Controller controller);
 
 }
